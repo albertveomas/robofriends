fa3af2179150940b73c47befc9beb5e014ba8e9d
Aung Myint Myat <aungmyintmyatiris@gmail.com>
2019-11-08 22:36:56 +0630

    Updates

---
+++ precache-manifest.0ea3e8186dcbc7e7343c47213011f3cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3718643b7c55b8be2dee72ec64bdc0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "217f26cdc55b0d683a76",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8d5bc0b5eadfd1813ef5",
+    "url": "/robofriends/static/css/main.ac2e69f8.chunk.css"
+  },
+  {
+    "revision": "217f26cdc55b0d683a76",
+    "url": "/robofriends/static/js/2.b97c6a0b.chunk.js"
+  },
+  {
+    "revision": "8d5bc0b5eadfd1813ef5",
+    "url": "/robofriends/static/js/main.92ab66de.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
